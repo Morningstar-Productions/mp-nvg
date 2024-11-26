ae86184688842cc94f42c6343627cd9692570452
xViperAG <usairsoftingandgaming@gmail.com>
2024-11-25 22:20:21 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,22 @@ description 'Night Vision / Thermal Vision Script for FiveM'
 author 'xViperAG'
 version '1.0.0'
 
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
 shared_scripts {
-    
+    '@ox_lib/init.lua',
+    '@Renewed-Lib/init.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+files {
+    'config.lua'
 }
